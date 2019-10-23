9e389314a5b0a44cf5b3a21f84d5559d542c93ba
sfunk02 <55893857+sfunk02@users.noreply.github.com>
2019-10-23 14:30:05 -0400

    Add files via upload

---
+++ sFunkServoControl.ino
@@ -0,0 +1,38 @@
+//Sam Funk
+//Servo Control
+//Uses two buttons to turn a servo in both directions.
+
+#include <Servo.h>    //Adds the servo library
+
+Servo myServo;    //Names the servo
+
+int servoPin = 9;         
+int btn1Pin = 11;
+int btn2Pin = 12;
+int btn1State = 0;
+int btn2State = 0;
+int angle = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(btn1Pin, INPUT);
+  pinMode(btn2Pin, INPUT);
+  myServo.attach(servoPin);
+}
+
+void loop() {
+  btn1State = digitalRead(btn1Pin);   
+  btn2State = digitalRead(btn2Pin);
+  if (btn1State == HIGH && angle < 180) {
+    Serial.println("Button 1 is on");
+    myServo.write(angle + 5);
+    angle = (angle + 5);
+  }
+  if (btn2State == HIGH && angle > 0) {
+    Serial.println("Button 2 is on");
+    myServo.write(angle - 5);
+    angle = (angle - 5);
+  }
+}
+
+      //Credit to: https://www.instructables.com/id/How-to-usa-a-Servo-motor-Arduino-Tutorial/
